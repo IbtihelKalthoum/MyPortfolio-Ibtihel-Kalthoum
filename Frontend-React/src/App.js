10a8722ec62d566a6e388ae09b168e9400407d96
Ibtihel Kalthoum <67706904+IbtihelKalthoum@users.noreply.github.com>
2023-09-29 10:06:34 +0100

    Add files via upload

---
+++ Frontend-React/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {About,Footer,Header,Skills,Work,Testimonial} from './container'
+
+import {Navbar} from './components'
+import './App.scss'
+
+function App() {
+
+
+
+
+  return (
+
+
+ <div className='app'>
+  <Navbar/>
+  <Header/>
+  <About/>
+  <Work/>
+  <Skills/>
+  <Testimonial/>
+  <Footer/>
+
+ </div>
+  
+  
+  
+  
+    )
+}
+
+export default App
